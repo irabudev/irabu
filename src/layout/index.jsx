c64e91a1032d1cc3db79a302f70f11170a764a68
mtuchi <mtuchidev@gmail.com>
2020-03-15 23:02:13 +0300

    style: add footer and imporve layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div className="layout-container">
+      <div>
         <Helmet>
           <meta name="description" content={config.siteDescription} />
           <html lang="en" />
         </Helmet>
         <Header />
-        {children}
+        <div className="site-wrapper-reveal">
+          {children}
+        </div>
+        <Footer />
       </div>
     );
   }
