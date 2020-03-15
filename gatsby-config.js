d39f75f98deba67d06a0afe6244260d3f6c29554
mtuchi <mtuchidev@gmail.com>
2020-03-15 10:19:03 +0300

    feat: add postcss and purgecss plugin for tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,23 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(`./tailwind.config.js`),
+          require(`autoprefixer`),
+          require(`cssnano`)
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: [`src/css/style.css`]
+      }
+    },
     "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-feed",
