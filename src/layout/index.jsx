2f5576ea185c4167b8b7dd44bdad1a236b2e574f
mtuchi <mtuchidev@gmail.com>
2020-03-15 20:01:26 +0300

    feat: masive clean up

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
-import "./index.css";
+import Header from "../components/Header"
 
 export default class MainLayout extends React.Component {
   render() {
@@ -12,6 +12,7 @@ export default class MainLayout extends React.Component {
           <meta name="description" content={config.siteDescription} />
           <html lang="en" />
         </Helmet>
+        <Header />
         {children}
       </div>
     );
