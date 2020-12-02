d36188da3c76e373338bdf30685b3377cec87d39
mtuchi <mtuchidev@gmail.com>
2020-12-02 17:35:48 +0300

    feat: update pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../layout";
-import Landing from "../components/Landing";
+import Hero from "../components/Hero";
 import Contact from "../components/Contact";
 import CaseStudy from "../components/CaseStudy";
+import Clients from "../components/Clients/Index";
 import OpenSource from "../components/OpenSource";
 import config from "../../data/SiteConfig";
 
-class HomePage extends Component {
-  render() {
-    return (
-      <Layout>
-        <Helmet title={`Home | ${config.siteTitle}`} />
-        <Landing />
-        <CaseStudy />
-        <OpenSource />
-        <Contact />
-      </Layout>
-    );
-  }
-}
+const HomePage = () => {
+  return (
+    <Layout>
+      <Helmet title={`Home | ${config.siteTitle}`} />
+      <Hero />
+      <CaseStudy />
+      <Clients />
+      <OpenSource />
+      <Contact />
+    </Layout>
+  );
+};
 
 export default HomePage;
